886f28b4709013cc54645addcfd7c62766240502
Raja Ahmad Mahmood <145890070+Ahmadbey678@users.noreply.github.com>
2025-12-22 18:28:09 +0500

    Time: 4 ms (64.31%), Space: 46.8 MB (23.86%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int romanToInt(String s) {
+        
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+
+        int n = s.length();
+
+        int ans = map.get(s.charAt(n-1));
+
+        int prev = ans;
+        for(int i = n-2; i >= 0; i--){
+
+            int curr = map.get(s.charAt(i));
+
+            if(curr < prev){
+                ans -= curr;
+            }
+            else{
+                ans += curr;
+            }
+            prev = curr;
+        }
+    return ans;
+
+    }
+}
