7689ddd590d75356598404894bbc800faf9c15d1
Raja Ahmad Mahmood <145890070+Ahmadbey678@users.noreply.github.com>
2026-07-01 16:09:45 +0500

    Time: 15 ms (44.29%), Space: 19.5 MB (45.25%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,24 @@
+class Solution:
+    def hashFunction(self,n):
+        return n-97
+
+    def isAnagram(self, s: str, t: str) -> bool:
+        arr1 = [0]*26
+        arr2 = [0]*26
+
+        if len(s) != len(t):
+            return False
+
+
+        for i in range(len(s)):
+
+            index1 = self.hashFunction(ord(s[i]))
+            arr1[index1]+=1
+
+            index2 = self.hashFunction(ord(t[i]))
+            arr2[index2]+=1
+
+        for i in range(26):
+            if arr1[i] != arr2[i]:
+                return False
+        return True      
